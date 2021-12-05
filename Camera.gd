90b1b99a54d77c4ca8f6be9cae212eabbc37e667
Caveweasel <52133772+Caveweasel@users.noreply.github.com>
2021-12-05 17:57:33 +0100

    Tweaked camera
    Camera now spawns in the middle of the map, no matter what map_size is, and will move faster. Also added a "sprint" button (shift) that makes the camera move even faster.

--- Camera.gd
+++ Camera.gd
@@ -1,5 +1,13 @@
 extends Camera2D
 
+var speed = 20
+
+
+func _ready():
+	var StartingPoint = get_parent().MAPSIZE * get_parent().get_child(1).cell_size / 2 #Finds the middle of the map
+	self.position = StartingPoint #Sets itself into the middle of the map
+
+
 func _input(event):
 	if Input.is_action_pressed("camera_zoom_in"): #and zoom > Vector2(0.25,0.25):
 		zoom -= Vector2(0.25,0.25) * zoom.x
@@ -8,11 +16,16 @@ func _input(event):
 
 
 func _process(delta):
+	if Input.is_action_pressed("camera_speed_up"):
+		speed = 40
+	else:
+		speed = 20
+	
 	if Input.is_action_pressed("camera_up") and not Input.is_action_pressed("camera_down"):
-		position.y -= 10 * zoom.x
+		position.y -= speed * zoom.x
 	if Input.is_action_pressed("camera_down") and not Input.is_action_pressed("camera_up"):
-		position.y += 10 * zoom.x
+		position.y += speed * zoom.x
 	if Input.is_action_pressed("camera_left") and not Input.is_action_pressed("camera_right"):
-		position.x -= 10 * zoom.x
+		position.x -= speed * zoom.x
 	if Input.is_action_pressed("camera_right") and not Input.is_action_pressed("camera_left"):
-		position.x += 10 * zoom.x
+		position.x += speed * zoom.x
