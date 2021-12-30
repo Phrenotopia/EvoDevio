0b1dda563fbcd8d1507c280f42077ff8f34478ec
Caveweasel <52133772+Caveweasel@users.noreply.github.com>
2021-12-30 21:29:41 +0100

    Camera changes and tiny grid tweak
    Camera now zooms towards the mouse and is unable to go over the edge. also tiny tweak to grid position.

--- Camera.gd
+++ Camera.gd
@@ -1,32 +1,67 @@
 extends Camera2D
 
 onready var world = get_node("/root/World")
-var speed = 20
+onready var world_size = world.MAPSIZE * world.PIXELSCALE
+onready var starting_point = world_size / 2 #Finds the middle of the map
 
+const SPEED = 10
+
+var current_speed = SPEED
 
 func _ready():
-	var StartingPoint = world.MAPSIZE * world.PIXELSCALE / 2 #Finds the middle of the map
-	self.position = StartingPoint #Sets itself into the middle of the map
+	self.position = starting_point #Sets itself into the middle of the map
+	zoom = world_size / Vector2(1920,1080) #Makes the camera fill the entire map
 
 
 func _input(event):
 	if Input.is_action_pressed("camera_zoom_in"): #and zoom > Vector2(0.25,0.25):
-		zoom -= Vector2(0.25,0.25) * zoom.x
+		#zoom -= Vector2(0.25,0.25) * zoom.x
+		if not zoom == Vector2(0.5,0.5):
+			zoom = Vector2(0.5,0.5)
+			position = get_global_mouse_position()
+		
+		#position -= get_global_mouse_position() - position / 2
+		
+	
 	elif Input.is_action_pressed("camera_zoom_out"): #and zoom < Vector2(4,4):
-		zoom += Vector2(0.25,0.25) * zoom.x
+		zoom = world_size / Vector2(1920,1080) #Makes the camera fill the entire map
+		self.position = starting_point #Puts the camera at the centre of the map
 
 
 func _process(delta):
+	movement()
+
+
+func movement():
+	
+	current_speed = SPEED*zoom.x
+	
 	if Input.is_action_pressed("camera_speed_up"):
-		speed = 40
+		
+		current_speed = current_speed*4 #Makes the camera go four times as fast when "sprinting"
+		
 	else:
-		speed = 20
+		current_speed = SPEED #Makes the camera go the normal speed
 	
 	if Input.is_action_pressed("camera_up") and not Input.is_action_pressed("camera_down"):
-		position.y -= speed * zoom.x
+		if position.y >= 540*zoom.y+current_speed: #I don't know why but this works. Makes the camera unable to leave the map
+			position.y -= current_speed
+		else:
+			position.y = 540*zoom.y
 	if Input.is_action_pressed("camera_down") and not Input.is_action_pressed("camera_up"):
-		position.y += speed * zoom.x
+		if position.y <= world_size.y - 540*zoom.y - current_speed:
+			position.y += current_speed
+		else:
+			position.y = world_size.y - 540*zoom.y
 	if Input.is_action_pressed("camera_left") and not Input.is_action_pressed("camera_right"):
-		position.x -= speed * zoom.x
+		if position.x >= 960*zoom.x+current_speed:
+			position.x -= current_speed
+		else:
+			position.x = 960*zoom.x
 	if Input.is_action_pressed("camera_right") and not Input.is_action_pressed("camera_left"):
-		position.x += speed * zoom.x
+		if position.x <= world_size.x - 960*zoom.x - current_speed:
+			position.x += current_speed
+		else:
+			position.x = world_size.x - 960*zoom.x
+	
+	#current_speed = SPEED #Makes sure the camera doesn't speed up over time when "sprinting"
--- DrawGrid.gd
+++ DrawGrid.gd
@@ -10,8 +10,8 @@ func _draw():
 		
 	for i in columns:
 		if i != 0 :
-			draw_rect(Rect2(i*world.GRIDSIZE-1, 0, 2, world.MAPSIZE.y*8), COLOUR) #Draws every line in the columns
+			draw_rect(Rect2(i*world.GRIDSIZE-2, 0, 2, world.MAPSIZE.y*world.PIXELSCALE), COLOUR) #Draws every line in the columns
 	for i in rows:
 		if i != 0 :
-			draw_rect(Rect2(-1, i*world.GRIDSIZE, world.MAPSIZE.x*8, 2), COLOUR) #Draws every line in the rows
+			draw_rect(Rect2(-1, i*world.GRIDSIZE-1, world.MAPSIZE.x*world.PIXELSCALE, 2), COLOUR) #Draws every line in the rows
 
